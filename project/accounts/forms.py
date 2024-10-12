b156fa5ce6339a8a73ed204c68f838cdf4a85aa5
tkt0605 <takato.komada2005@outlook.jp>
2024-10-12 21:51:56 +0900

    CustomUserでのwidgetの作成

--- project/accounts/forms.py
+++ project/accounts/forms.py
@@ -4,27 +4,12 @@ from allauth.account.forms import SignupForm
 from .models import CustomUser
 
 class CustomUserCreationForm(SignupForm):
-    last_name = forms.CharField(max_length=30, label='名字')
-    first_name = forms.CharField(max_length=30, label='名前')
+    first_name = forms.CharField(max_length=8, label='姓',widget=forms.TextInput(attrs={'placeholder':'姓', 'class':'first_name'}))
+    last_name = forms.CharField(max_length=8, label='名',widget=forms.TextInput(attrs={'placeholder':'名', 'class':'last_name'}))
     class Meta:
         model = CustomUser
         fields = ['email', 'username', 'first_name', 'last_name', 'password1', 'password2']
-        widgets = {
-            'first_name': forms.TextInput(
-                attrs={
-                    "class": "first_name",
-                    "placeholder": "姓",
-                    "max-length": 8,
-                }
-            ),
-            'last_name': forms.TextInput(
-                attrs={
-                    'class': 'last_name',
-                    "placeholder": "名",
-                    "max-length": 8
-                }
-            ),
-        }
+
     def signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
